b9bd58240785134626dd2f820ef93628ef66fc17
Rustam38 <81112479+Rustam38@users.noreply.github.com>
2025-12-20 14:44:50 +0300

    Add files via upload

---
+++ DO5_SimpleDocker-1-develop/src/screen/server/mini_server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <fcgi_stdio.h>
+
+int main(){
+	while (FCGI_Accept() >= 0) {
+		printf("Content-type: text/html\r\n\r\n");
+		printf("<html><head><title>Hello World</title></head>");
+		printf("<body><h1>Hello World!</h1></body>");
+		printf("</html>");
+	}
+	return 0;
+}
